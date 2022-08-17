c4b45c63775c3ee75e75d8a0cd353e6e33d8723f
LuanNg <thithaicm1@gmail.com>
2022-08-18 00:04:33 +0700

    change layout component directory 2th

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
-import Layout from "./layout";
+import Layout from "../Components/layout";
 import { useEffect, useState } from "react";
 import React from "react";
 
